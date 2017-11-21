e3b2921423a6a4b0de0d13c420c936199b027caa
StormSpirit22 <icestormspirit@gmail.com>
2017-11-21 18:52:42 +0800

    update web3

--- src/utils/global/index.js
+++ src/utils/global/index.js
@@ -1,5 +1,6 @@
 import events from 'events'
 import io from 'socket.io-client'
+import { default as Web3} from 'web3';
 
 export const proxy = new events.EventEmitter();
 
@@ -24,4 +25,16 @@ global.newSocket = () => {
   socket.on('connect_error', err => console.log(err))
   socket.on('disconnect', () => console.log('socket disconnect'))
   return socket
+}
+
+global.getWeb3 = () => {
+  window.addEventListener('load', function() {
+    if (typeof web3 !== 'undefined') {
+      window.web3 = new Web3(web3.currentProvider);
+    } else {
+      console.log('No web3? You should consider trying MetaMask!')
+      window.web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+    }
+  })
+  return window.web3;
 }
